806128bd506ea3796b0808b111977f1923e40d71
RohitBK201 <rohitnrdtamangBK201@gmail.com>
2022-04-28 07:20:22 +0530

    stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Stwh } from './component/stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Stwh/>
+
+    </div>
+  );
+}
+
+export default App;
